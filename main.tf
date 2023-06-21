681169de03b0784f882e5442e17ae3341df236a8
Rogerio Calixto <rpcalixtosistemas@gmail.com>
2023-06-20 21:25:20 -0300

    passing user-data parameter

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "main-network" {
 }
 
 data "template_file" "user_data_manager" {
-  template = file("scripts/ansible_clients.sh")
+  template = file("scripts/ansible_client.sh")
   # vars = {
   #   environment = var.environment
   # }
@@ -37,16 +37,16 @@ data "template_file" "user_data_client" {
   #   environment = var.environment
   # }
 }
-# module "ec2-client" {
-#   source            = "../terraform-aws-ec2"
-#   user-data         = data.template_file.user_data_client.rendered
-#   servers           = 3
-#   region            = var.region
-#   ami               = var.ami
-#   instance-type     = var.instance-type
-#   instance-name     = "${var.instance-name}-ansible_client"
-#   keypair-name      = var.keypair-name
-#   authorized-ssh-ip = ["${var.authorized-ssh-ip}/32", "${module.ec2-manager.ec2_private_ips[0]}/32"]
-#   vpc-id            = module.main-network.vpc-id
-#   subnet-ids        = module.main-network.public-subnet-ids
-# }
+module "ec2-client" {
+  source            = "../terraform-aws-ec2"
+  user-data         = data.template_file.user_data_client.rendered
+  servers           = 3
+  region            = var.region
+  ami               = var.ami
+  instance-type     = var.instance-type
+  instance-name     = "${var.instance-name}-ansible_client"
+  keypair-name      = var.keypair-name
+  authorized-ssh-ip = ["${var.authorized-ssh-ip}/32", "${module.ec2-manager.ec2_private_ips[0]}/32"]
+  vpc-id            = module.main-network.vpc-id
+  subnet-ids        = module.main-network.public-subnet-ids
+}
